15365840bff977f14a1d0707ed50e6231c149e9b
Maman Khan <mamankhan99@gmail.com>
2018-08-01 19:12:04 +0500

    My first research.

---
+++ Starting with C Sharp/Program.cs
@@ -0,0 +1,24 @@
+using System;
+
+
+namespace Hello_World
+{   
+    class Program
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+  
+        static void Main()
+        {
+            /*Console.Write("Enter a number: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nThe sum of two numbers is : {0} \n", Sum(n1, n2));*/
+            string str = "s32d443";
+            Console.WriteLine(str);
+        }
+    }
+}
