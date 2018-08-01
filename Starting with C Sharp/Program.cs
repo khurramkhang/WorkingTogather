0e10d2c762ec9d9678f4fcb490ccc0553349e15f
Maman Khan <mamankhan99@gmail.com>
2018-08-01 19:36:31 +0500

    New feature.

--- Starting with C Sharp/Program.cs
+++ Starting with C Sharp/Program.cs
@@ -5,20 +5,24 @@ namespace Hello_World
 {   
     class Program
     {
-        public static int Sum(int a, int b)
+        static int Sum(int a, int b)
         {
             return a + b;
         }
-  
+
+        static int Subtraction(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
         static void Main()
         {
-            /*Console.Write("Enter a number: ");
+            Console.Write("Enter a number: ");
             int n1 = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter another number: ");
             int n2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\nThe sum of two numbers is : {0} \n", Sum(n1, n2));*/
-            string str = "s32d443";
-            Console.WriteLine(str);
+            Console.WriteLine("\nThe sum of two numbers is : {0} \n", Subtraction(n1, n2));
+            
         }
     }
 }
